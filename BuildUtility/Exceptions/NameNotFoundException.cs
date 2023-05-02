9ef14aee54820656fc5a07c0cd4543d2044940a7
sanek2bas <sanek_2bas@mail.ru>
2023-05-02 23:30:24 +0300

    some rafctoring program.cs

---
+++ BuildUtility/Exceptions/NameNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿namespace BuildUtility.Exceptions
+{
+    public sealed class NameNotFoundException : Exception
+    {
+        private const string NameWasNotFound = "Name was not found";
+
+        public NameNotFoundException() :
+               base($"{NameWasNotFound}.")
+        {
+        }
+
+        public NameNotFoundException(string message) :
+               base($"{NameWasNotFound}: " + message)
+        {
+        }
+
+        public NameNotFoundException(string message, Exception inneException) :
+               base($"{NameWasNotFound}: " + message, inneException)
+        {
+        }
+    }
+}
--- BuildUtility/Program.cs
+++ BuildUtility/Program.cs
@@ -1,13 +1,34 @@
 ﻿using BuildUtility.Entity;
+using BuildUtility.Exceptions;
 using BuildUtility.Parser;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Runtime.CompilerServices;
 
-const string FILE_NAME = "makefile.txt";
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string FILE_NAME = "makefile.txt";
 
-var parser = new FileParser();
-var t = parser.Parse(FILE_NAME);
+        try
+        {
+            if (args.Length != 1)
+                throw new ArgumentException("You only need to provide a name of target build");
+            var targetBuild = args[0];
+            var parser = new FileParser();
+            var builds = parser.Parse(FILE_NAME);
+            if (builds.All(b => b.Name != targetBuild))
+                throw new NameNotFoundException(targetBuild);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.WriteLine("Try again please...");
+        }
+     
 
-Console.ReadKey();
+        Console.ReadKey();
+    }
+}
