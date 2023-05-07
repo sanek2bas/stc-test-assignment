3bcfa59411706c1afc58df0edb63b52eb40ddd1f
sanek2bas <sanek_2bas@mail.ru>
2023-05-08 00:30:17 +0300

    final commit

--- BuildUtility.UnitTests/GraphTests.cs
+++ BuildUtility.UnitTests/GraphTests.cs
@@ -1,4 +1,5 @@
-﻿using BuildUtility.Structures;
+﻿using BuildUtility.Exceptions;
+using BuildUtility.Structures;
 using NUnit.Framework;
 
 namespace BuildUtility.UnitTests
@@ -9,6 +10,7 @@ namespace BuildUtility.UnitTests
         private const string NODE_1 = "NODE_1";
         private const string NODE_2 = "NODE_2";
         private const string NODE_3 = "NODE_3";
+        private const string NODE_4 = "NODE_4";
 
         private Dictionary<string, INode<string>> nodesDic;
 
@@ -34,12 +36,47 @@ namespace BuildUtility.UnitTests
             Assert.That(result[2], Is.EqualTo(node1));
         }
 
+        [Test]
+        public void Correct_Simple_Graph_Circular_Dependency_Test()
+        {
+            var node1 = createNode(NODE_1);
+            var node2 = createNode(NODE_2);
+            var node3 = createNode(NODE_3);
+            node1.DependenciesKey.Add(node2.Key);
+            node1.DependenciesKey.Add(node3.Key);
+            node2.DependenciesKey.Add(node3.Key);
+            node3.DependenciesKey.Add(node2.Key);
+
+            Assert.Throws<СircularDependencyException>(() =>
+                Graph<string>.DFS(node1, nodesDic));
+        }
+
         private Node createNode(string key)
         {
             var node = new Node(key);
             nodesDic.Add(key, node);
             return node;
         }
+
+        [Test]
+        public void Correct_Difficult_Graph_Test()
+        {
+            var node1 = createNode(NODE_1);
+            var node2 = createNode(NODE_2);
+            var node3 = createNode(NODE_3);
+            var node4 = createNode(NODE_4);
+            node1.DependenciesKey.Add(node2.Key);
+            node1.DependenciesKey.Add(node4.Key);
+            node2.DependenciesKey.Add(node3.Key);
+            node4.DependenciesKey.Add(node3.Key);
+
+            var result = Graph<string>.DFS(node1, nodesDic);
+            Assert.That(result[0], Is.EqualTo(node3));
+            Assert.That(result[1], Is.EqualTo(node4));
+            Assert.That(result[2], Is.EqualTo(node2));
+            Assert.That(result[3], Is.EqualTo(node1));
+        }
+
 
         private class Node : INode<string>
         {
--- BuildUtility/Program.cs
+++ BuildUtility/Program.cs
@@ -13,14 +13,29 @@ internal class Program
         {
             if (args.Length != 1)
                 throw new ArgumentException("You only need to provide a name of target build");
-            
-            var targetBuild = args[0];
-            var parser = new FileParser();
-            var builds = parser.Parse(FILE_NAME);
-            
-            if (builds.All(b => b.Name != targetBuild))
-                throw new NameNotFoundException(targetBuild);
 
+            var targetBuildName = args[0];
+            var parser = new FileParser();
+
+            Build? targetBuild = null;
+            var buildsDic = new Dictionary<string, INode<string>>();
+            foreach(var build in parser.Parse(FILE_NAME))
+            {
+                if(build.Key == targetBuildName)
+                    targetBuild = build;
+                buildsDic.Add(build.Key, build);
+            }
+            
+            if (targetBuild == null)
+                throw new NameNotFoundException(targetBuildName);
+
+            var orderedBuilds = Graph<string>.DFS(targetBuild, buildsDic);
+            foreach(Build build in orderedBuilds.Cast<Build>())
+            {
+                
+                foreach(var action in build.Actions)
+                Console.WriteLine(action);
+            }
         }
         catch (Exception ex)
         {
